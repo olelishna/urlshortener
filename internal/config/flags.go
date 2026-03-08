7f3d905d62018f53fc94213e64dacdca0598f866
Olga Svistunova <svistunovaoliya@yandex.ru>
2026-03-08 14:43:52 +0300

    iter4: rename variable

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -5,10 +5,10 @@ import (
 )
 
 var FlagRunAddr string
-var FlagBaseUrlResult string
+var FlagBaseURLResult string
 
 func ParseFlags() {
 	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
-	flag.StringVar(&FlagBaseUrlResult, "b", "http://localhost:8080/", "base address for shortened url")
+	flag.StringVar(&FlagBaseURLResult, "b", "http://localhost:8080/", "base address for shortened url")
 	flag.Parse()
 }
